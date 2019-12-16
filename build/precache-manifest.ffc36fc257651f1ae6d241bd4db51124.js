8839e44cae813a849084a0f1fc95848a65a6f4bd
annamaria <annamaria.wojtyga@gmail.com>
2019-12-16 17:27:45 +0100

    npm run build

---
+++ build/precache-manifest.ffc36fc257651f1ae6d241bd4db51124.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "f797ad6fe500a814f860",
+    "url": "/static/css/main.d9fe43eb.chunk.css"
+  },
+  {
+    "revision": "f797ad6fe500a814f860",
+    "url": "/static/js/main.f797ad6f.chunk.js"
+  },
+  {
+    "revision": "ecebb60d70e42754ec37",
+    "url": "/static/css/1.65d24d6e.chunk.css"
+  },
+  {
+    "revision": "ecebb60d70e42754ec37",
+    "url": "/static/js/1.ecebb60d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3aa4e5413ab11d442a2dd40ff097d206",
+    "url": "/static/media/me.3aa4e541.png"
+  },
+  {
+    "revision": "a2a0cf53fabc964c3ab35f4c974fd699",
+    "url": "/static/media/wojtyga-en.a2a0cf53.pdf"
+  },
+  {
+    "revision": "1a04dac58ac9a8ddcfe76169c3b1bedb",
+    "url": "/static/media/clf.1a04dac5.png"
+  },
+  {
+    "revision": "233d37877aeb88d71ca708728ab53945",
+    "url": "/static/media/dataprotec.233d3787.png"
+  },
+  {
+    "revision": "b7f5a711cb8ebbeb536a4db22c774f36",
+    "url": "/static/media/kidaroo.b7f5a711.png"
+  },
+  {
+    "revision": "b6ee18b157e030f3d7ebbff705a083e3",
+    "url": "/static/media/displacements.b6ee18b1.png"
+  },
+  {
+    "revision": "1ec57179eb0fa31569561cc64f33fad5",
+    "url": "/static/media/githubsearch.1ec57179.png"
+  },
+  {
+    "revision": "49f81f09543aed1a432d71d2ccdc44d0",
+    "url": "/static/media/ts.49f81f09.png"
+  },
+  {
+    "revision": "2d8b63cca25a6719529409932a951aa8",
+    "url": "/static/media/background.2d8b63cc.svg"
+  },
+  {
+    "revision": "c292947f8498c728382638e46efc6e19",
+    "url": "/index.html"
+  }
+];
